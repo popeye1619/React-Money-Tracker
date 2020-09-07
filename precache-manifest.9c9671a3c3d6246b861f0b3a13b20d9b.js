166b352f35246798f7aebe9f57417bbc8a12783c
popeye1619 <abhowmik14@gmail.com>
2020-09-07 18:17:11 +0530

    Updates

---
+++ precache-manifest.9c9671a3c3d6246b861f0b3a13b20d9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442dbb4174d9c406ec6e259cb15c2156",
+    "url": "/React-Money-Tracker/index.html"
+  },
+  {
+    "revision": "2f90538f504551694a1f",
+    "url": "/React-Money-Tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6b4fbd364b80858ea3d3",
+    "url": "/React-Money-Tracker/static/js/2.6dfdf6a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Money-Tracker/static/js/2.6dfdf6a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f90538f504551694a1f",
+    "url": "/React-Money-Tracker/static/js/main.9e615edc.chunk.js"
+  },
+  {
+    "revision": "fb7e97cd6245a8a1cab2",
+    "url": "/React-Money-Tracker/static/js/runtime-main.3bb23edf.js"
+  }
+]);
